4c70cb8279333eb09dbc2daa9def2e01bce87784
ahsanreal4 <ahsan.btph123@gmail.com>
2023-04-10 15:26:36 +0500

    tokenization

--- lexer.ts
+++ lexer.ts
@@ -1,1 +1,85 @@
-console.log("Interpreter");
+export enum TokenType {
+  Number,
+  Identifier,
+  Equals,
+  OpenParem,
+  CloseParem,
+  BinaryOperator,
+  Let,
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+  let: TokenType.Let,
+};
+
+export interface Token {
+  value: string;
+  type: TokenType;
+}
+
+function token(value: string, type: TokenType): Token {
+  return { value, type };
+}
+
+function isBinaryOperator(char: string): boolean {
+  return char === "+" || char === "-" || char === "*" || char === "/";
+}
+
+function isAlpha(src: string): boolean {
+  return src.toUpperCase() !== src.toLocaleLowerCase();
+}
+
+function isInt(src: string): boolean {
+  return /^-?\d+$/.test(src);
+}
+
+function isSkippable(src: string): boolean {
+  return src === " " || src === "\n" || src === "\t";
+}
+
+export function tokenize(sourceCode: string): Token[] {
+  const tokens = new Array<Token>();
+  const src: string[] = sourceCode.split("");
+
+  // Build each token until end of source code
+  while (src.length > 0) {
+    if (src[0] === "(") {
+      tokens.push(token(src.shift() ?? "", TokenType.OpenParem));
+    } else if (src[0] === ")") {
+      tokens.push(token(src.shift() ?? "", TokenType.CloseParem));
+    } else if (isBinaryOperator(src[0])) {
+      tokens.push(token(src.shift() ?? "", TokenType.BinaryOperator));
+    } else if (src[0] === "=") {
+      tokens.push(token(src.shift() ?? "", TokenType.Equals));
+    } else {
+      // Handle Multi character tokens
+
+      if (isInt(src[0])) {
+        let num = "";
+        while (src.length > 0 && isInt(src[0])) {
+          num += src.shift();
+        }
+        tokens.push(token(num, TokenType.Number));
+      } else if (isAlpha(src[0])) {
+        let ident = "";
+        while (src.length > 0 && isAlpha(src[0])) {
+          ident += src.shift();
+        }
+
+        // Check for reserved keywords first
+        const reserved = KEYWORDS[ident];
+        if (reserved === undefined) {
+          tokens.push(token(ident, TokenType.Identifier));
+        } else {
+          tokens.push(token(ident, reserved));
+        }
+      } else if (isSkippable[src[0]]) {
+        src.shift();
+      } else {
+        throw new Error(`Unrecognized character found in source: ${src[0]}`);
+      }
+    }
+  }
+
+  return tokens;
+}
